8c907c95002378492011f7707b6fa53dc3d459de
RDF <foulkesr@gmail.com>
2017-08-11 09:14:39 -0500

    production port

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 8080);, function () {
   console.log('Example app listening on port 3000!')
 })
